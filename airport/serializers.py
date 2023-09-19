3f31821b6f2ba4bb09541e3ec986fe65f8920942
misha <mishabenzel1997@gmail.com>
2023-09-19 21:14:45 +0200

    Added AirplaneSerializer, AirplaneViewSet & url for Airplane

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from airport.models import AirplaneType
+from airport.models import AirplaneType, Airplane, Airport
 
 
 class AirplaneTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = AirplaneType
         fields = ("id", "name")
+
+
+class AirplaneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airplane
+        fields = ("id", "name", "rows", "seats_in_row", "airplane_type", "capacity")
--- airport/views.py
+++ airport/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 
-from airport.models import AirplaneType
-from airport.serializers import AirplaneTypeSerializer
+from airport.models import AirplaneType, Airplane, Airport
+from airport.serializers import AirplaneTypeSerializer, AirplaneSerializer, AirportSerializer
 
 
 class AirplaneTypeViewSet(viewsets.ModelViewSet):
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
