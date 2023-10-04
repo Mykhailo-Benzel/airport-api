b9560e3e1a64da403d28bdbc51434e8ca3134c23
misha <mishabenzel1997@gmail.com>
2023-10-04 13:58:30 +0200

    Added throttling

--- airport-service/settings.py
+++ airport-service/settings.py
@@ -140,6 +140,11 @@ INTERNAL_IPS = [
 REST_FRAMEWORK = {
     # YOUR SETTINGS
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "10/day", "user": "100/day"},
 }
 
 SPECTACULAR_SETTINGS = {
